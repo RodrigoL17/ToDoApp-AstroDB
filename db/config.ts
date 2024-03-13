5dc62abe93ca62ba1f731c0b9a214571489dae10
Rodrigo Lezama <cordolezama@gmail.com>
2024-03-13 13:08:14 -0300

    probando

--- db/config.ts
+++ db/config.ts
@@ -1,25 +1,25 @@
-import { NOW, column, defineDb, defineTable } from 'astro:db';
+import { NOW, column, defineDb, defineTable,  } from 'astro:db';
 
 const Task = defineTable({
   columns: {
-    id: column.number({primaryKey: true}),
+    id: column.number({primaryKey: true, unique: true}),
     title: column.text(),
     description: column.text({}),
     created_at: column.date({default: NOW}),
-    authorId: column.number({ references: () => Author.columns.id }),
+    // foreingKeys: {},
   }
 })
 
-const Author = defineTable({
-  columns: {
-    id: column.number({primaryKey: true}),
-    name: column.text(),
-    lastname: column.text(),
-    fullName: column.text()
-  }
-})
+// const Author = defineTable({
+//   columns: {
+//     id: column.number({primaryKey: true, unique: true}),
+//     name: column.text(),
+//     lastname: column.text(),
+//     fullName: column.text()
+//   }
+// })
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {Task, Author}
+  tables: {Task}
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,9 @@
-import { db } from 'astro:db';
+import { db, Task } from 'astro:db';
 
-// https://astro.build/db/seed
-export default async function seed() {
-	// TODO
-}
+export default async function() {
+	await db.insert(Task).values([
+	 {id: 1, title: 'Task 1', description: 'Description 1'},
+	 {id: 2, title: 'Task 2', description: 'Description 2'},
+	 {id: 3, title: 'Task 3', description: 'Description 3'},
+	])
+  }
